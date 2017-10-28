adccaee15b524d4127a3175b255e9174a6306b64
a-yun <antony.n.yun@gmail.com>
2017-10-28 16:06:27 -0500

    Add endpoints for managing offers

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -2,6 +2,22 @@ import { Router } from 'express';
 import * as PostController from '../controllers/post.controller';
 const router = new Router();
 
+// Get all offers for a user
+router.route('/offers/:user').get(PostController.getOffers);
+
+// Get details for one offer by cuid
+router.route('/offers/:user/:cuid').get(PostController.getOffer);
+
+// Add an offer to a user
+router.route('/offers/:user').post(PostController.addOffer);
+
+// Edit a user's offer by cuid
+router.route('/offers/:user/:cuid').post(PostController.editOffer);
+
+// Delete an offer by cuid
+router.route('/offers/:user/:cuid').delete(PostController.deleteOffer);
+
+// TODO: remove old endpoints
 // Get all Posts
 router.route('/posts').get(PostController.getPosts);
 
